a1d9749c4ca0cb63fcbe6a81beb91364f144b021
Barba <vinicius.gomes@ramper.com.br>
2022-04-07 13:47:47 -0300

    arrumando imports

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_item";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
 
 const customer = new Customer("123", "vini barba");
 const address = new Address("Rua test", 42, "03099090", "São Paulo");
