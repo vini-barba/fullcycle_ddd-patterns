4a6665d6d13954209ebf27346064bda822e6fd39
Barba <vinicius.gomes@ramper.com.br>
2022-04-13 09:14:21 -0300

    test de unidade do FindUserUseCase

---
+++ src/usecase/customer/find/find.customer.unit.spec.ts
@@ -0,0 +1,38 @@
+import Customer from "../../../domain/customer/entity/customer";
+import Address from "../../../domain/customer/value-object/address";
+import FindCustomerUseCase from "./find.customer";
+
+describe("Unit test find customer use case", () => {
+  const customer = new Customer("123", "john");
+  const address = new Address("street", 123, "zip", "city");
+  customer.changeAddress(address);
+
+  const MockRepository = () => {
+    return {
+      find: jest.fn().mockResolvedValue(customer),
+      findAll: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+    };
+  };
+
+  it("Should find a customer", async () => {
+    const customerRepository = MockRepository();
+    const usecase = new FindCustomerUseCase(customerRepository);
+    const input = { id: "123" };
+    const output = {
+      id: customer.id,
+      name: customer.name,
+      address: {
+        street: address.street,
+        city: address.city,
+        number: address.number,
+        zip: address.zip,
+      },
+    };
+
+    const result = await usecase.execute(input);
+
+    expect(result).toStrictEqual(output);
+  });
+});
